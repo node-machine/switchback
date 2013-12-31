2a7e8d9efe2ebd41b13151bf57a87d6ad26b9010
Mike McNeil <michael.r.mcneil@gmail.com>
2013-12-31 17:42:58 -0600

    Added isSwitchback() method, passes test.

--- test/usage.test.js
+++ test/usage.test.js
@@ -1,29 +1,50 @@
 var should = require('should');
 
-describe('switcher(Function)', function (){
-	it('should return a switchback', function () {
 
+var switchback = require('../lib');
+
+
+// Fixtures
+var fixtures = {
+	fn: function (err, data) {},
+	handlers: {
+		success: function (data) {},
+		error: function (err) {}
+	}
+};
+
+
+// Suites
+describe('switchback(Function)', function (){
+	
+	var cb = fixtures.fn;
+
+	it('should return a switchback', function () {
+		var sb = switchback(cb);
+
+		sb.should.be.a.Function;
+		switchback.isSwitchback(sb).should.be.ok;
 	});
 });
 
-describe('switcher(Handlers)', function (){
+describe('switchback(Handlers)', function (){
 	
 });
 
-describe('switcher(Function, Object)', function (){
+describe('switchback(Function, Object)', function (){
 	
 });
 
-describe('switcher(Handlers, Object)', function (){
+describe('switchback(Handlers, Object)', function (){
 	
 });
 
 
-describe('switcher(Function, Object, Object)', function (){
+describe('switchback(Function, Object, Object)', function (){
 	
 });
 
-describe('switcher(Handlers, Object, Object)', function (){
+describe('switchback(Handlers, Object, Object)', function (){
 	
 });
 
